5945731b726a688d2178eca469062311df5d2791
jonatan <jperezmendez@bostonk12.org>
2025-01-09 10:32:17 -0500

    tarea de hoy 1/9/25

---
+++ 2024-2025/Python/Week_9/ 2025-01-09/if.py
@@ -0,0 +1,9 @@
+#jonatan
+#2025-01-09
+
+num1=int(input("Please enter a number"))
+num2=int(input("Please enter another number"))
+if num1>num2:
+   print("The biggest number that I have seen so far is ",num1)
+else:
+    print("The biggest number that I have seen so far is ",num2)
